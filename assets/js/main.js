85dd4dca8621c9e8c399f7704140d028abecba51
Matias <matias.garcia1994@hotmail.com>
2023-03-08 22:05:53 -0300

    change form method

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,5 +21,18 @@ function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
-
-
+function sendEmail() {
+  Email.send({
+    Host : "smtp.elasticemail.com",
+    SecureToken : "88247c5d-246e-454c-93d0-73878a89277c",
+    To : 'mrg.94.grc@gmail.com',
+    From : document.getElementById("email").value,
+    Subject : document.getElementById("subject").value,
+    Body : "Name : " + document.getElementById("name").value
+      + ", Email : " + document.getElementById("email").value
+      + ", Subject : " + document.getElementById("subject").value
+      + ", Message : " + document.getElementById("message").value
+  }).then(
+  message => alert(message)
+  );
+}
